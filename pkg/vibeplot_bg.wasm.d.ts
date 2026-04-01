9b497c8fb0cdfbfec39ae0c8689cdcc6a24cea05
shreyaspotnis <shreyaspotnis@users.noreply.github.com>
2026-04-01 01:20:04 +0000

    deploy: c2aa0bd2facfa35ea5bdfc5995801141ea19d2fd

---
+++ pkg/vibeplot_bg.wasm.d.ts
@@ -0,0 +1,27 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const get_rotation: () => any;
+export const load_cube_model: () => [number, number];
+export const load_model: (a: number, b: number) => [number, number];
+export const load_pyramid_model: () => [number, number];
+export const reset_rotation: () => void;
+export const reset_zoom: () => void;
+export const run: () => void;
+export const set_rotation: (a: number, b: number) => void;
+export const set_transparent_mode: (a: number) => void;
+export const set_zoom: (a: number) => void;
+export const get_zoom: () => number;
+export const wasm_bindgen__closure__destroy__h0961b39a1ced778b: (a: number, b: number) => void;
+export const wasm_bindgen__closure__destroy__hd83c1c2416b3630d: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h7d9d5be39643ee4e: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h1115b2d2f01508be: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__hbba38c098e4f641f: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
